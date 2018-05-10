088949b9f68dfad7fe4c3a39746c01531ffd8240
Chemley <canaowens@gmail.com>
2018-05-10 15:01:51 -0400

    to do list lab complete

---
+++ controllers/todo-controller.js
@@ -0,0 +1,43 @@
+(function () {
+  function TodoController() {
+
+/* TODO: controller must add an array of sample todo objects to the view model.
+  Each todo object has “task” (string) and “completed” (boolean).
+  Include a mix of complete and incomplete items. */
+    var vm = this;
+
+   vm.todoList = [
+     {task: "Wash Clothes",
+    completed: false},
+     {task: "Do Dishes",
+    completed: true},
+     {task: "Take Out Dog",
+    completed: false}
+   ];
+   vm.newTaskText = "";
+
+
+    vm.addTodo = function(task) {
+      console.log(task);
+      vm.todoList.push({
+        task: task,
+        completed: false
+      });
+      // clears the input after a new task is added.
+      vm.newTaskText = "";
+    };
+
+    vm.deleteTodo = function(task) {
+      vm.todoList.splice(task, 1);
+    }
+
+    vm.completeTodo = function(task) {
+      task.completed = true;
+    }
+  }
+
+
+  angular
+    .module("todoApp")
+    .controller("TodoController", TodoController);
+})();
